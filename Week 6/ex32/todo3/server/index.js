3d291de56736330dce5fc3f0666d6fa2c1276025
Henry Gill <caatius@gmail.com>
2022-10-17 21:35:13 +0300

    Todo3 done

--- Week 6/ex32/todo3/server/index.js
+++ Week 6/ex32/todo3/server/index.js
@@ -27,4 +27,26 @@ app.get("/",async function (req, res) {
     }
 })
 
+app.post("/new", async function (req, res) {
+    try {
+        const connection = await mysql.createConnection(config.db)
+        // Execute prepared statement.
+        const [result,] = await connection.execute('insert into task (description) values (?) ', [req.body.description])
+        res.status(200).json({id:result.insertId})
+    } catch(err) {
+        res.status(500).json({error: err.message})
+    }
+})
+
+app.delete("/delete/:id", async function (req, res) {
+    try {
+        const connection = await mysql.createConnection(config.db)
+        // Execute prepared statement.
+        await connection.execute('delete from task where id = ? ', [req.params.id])
+        res.status(200).json({id:req.params.id})
+    } catch(err) {
+        res.status(500).json({error: err.message})
+    }
+})
+
 app.listen(port)
