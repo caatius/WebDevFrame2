cc74cd0dae52df679b611eb381ac820e69abf234
Henry Gill <caatius@gmail.com>
2022-10-18 14:15:10 +0300

    Todo6 done

---
+++ Week 7/ex35/todo6/src/App.js
@@ -0,0 +1,119 @@
+import './App.css';
+import axios from 'axios';
+import {
+  useEffect,
+  useState
+} from 'react';
+
+const URL = 'http://localhost:3001/'
+
+
+
+function App() {
+  const [tasks, setTasks] = useState([])
+  const [task, setTask] = useState('')
+  const [editTask, setEditTask] = useState(null)
+  const [editDescription, setEditDescription] = useState('')
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response) => {
+      setTasks(response.data)
+    }).catch(error => {
+      alert(error.response.data.error)
+    })
+  }, [])
+
+  function setEditableRow(task) {
+    setEditTask(task)
+    setEditDescription(task.description)
+  }
+
+function edit() {
+  const json = JSON.stringify({id: editTask.id, description: editDescription})
+  axios.put(URL + 'edit', json,{
+    headers: {
+      'Content-Type' : 'application/json'
+    }
+  })
+  .then((response) => {
+    // Convert stringifyed JSON object back to Javascript object.
+    const editedObject = JSON.parse(json)
+    // Create copy of tasks state variable.
+    const tempArray = [...tasks]
+    // Find task that was being edited.
+    const index = tempArray.findIndex(task => {return task.id === editTask.id})
+    // If found, update desc.
+    if (index !==-1) tempArray[index].description = editDescription
+    // Update state containing list of tasks.
+    setTasks(tempArray)
+    setEditTask(null)
+    setEditDescription('')
+  }).catch(error => {
+    alert(error.response.data.error)
+  })
+}
+
+  function save() {
+    const json = JSON.stringify({description: task})
+    axios.post(URL + 'new',json,{
+      headers: {
+        'Content.Type' : 'application/json'
+      }
+  })
+  .then((response) => {
+    // Convert stringifyed JSON object back to JS object.
+    const addedObject = JSON.parse(json)
+    // Add id returned by the server to object.
+    addedObject.id = response.data.id
+    // Update state variable with newly added data.
+    setTasks(tasks => [...tasks, addedObject])
+    setTask('')
+  }).catch(error => {
+    alert(error.response.data.error)
+  })
+  }
+
+  function remove(id) {
+    axios.delete(`${URL}delete/${id}`)
+    .then(() => {
+      const newListWithoutRemoved = tasks.filter((item) => item.id !==id)
+      setTasks(newListWithoutRemoved)
+    }).catch (error => {
+      alert(error.response.data.error)
+    })
+  }
+
+  return (
+      <div style={{margin: '20px'}}>
+        <h3>My tasks</h3>
+          <form>
+            <label>Add new</label>
+            <input value={task} onChange={e => setTask(e.target.value)} />
+            <button type='button' onClick={save}>Save</button>
+          </form>
+          <ol>
+            {tasks.map(task => (
+              <li key={task.id}>
+                {editTask?.id !== task.id &&
+                  task.description + ' '
+                }
+                {editTask?.id === tasks.id &&
+                  <form>
+                    <input value={editDescription} onChange={e => setEditDescription(e.target.value)} />
+                    <button type="button" onClick={edit}>Save</button>
+                    <button type="button" onClick={() => setEditTask(null)}>Cancel</button>
+                  </form>
+                  }
+                  <a href="/#" onClick={() => remove(task.id)}>Delete</a>&nbsp;
+                  {editTask === null &&
+                    <a href="/#" onClick={() => setEditableRow(task)}>Edit</a>
+                  }
+                </li>
+          ))}
+          </ol>
+      </div>
+  );
+}
+
+export default App;
